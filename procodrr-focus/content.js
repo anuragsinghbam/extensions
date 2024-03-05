add0165ba6ea39458aa2d5378d9386b0b194084b
Anurag Singh <61440053+anuragsinghbam@users.noreply.github.com>
2024-03-05 18:39:26 +0530

    Add files via upload

---
+++ procodrr-focus/content.js
@@ -0,0 +1,6 @@
+// const playerOrigin = new URLSearchParams(location.search).get('origin')
+// const isProCodrrPlayer = playerOrigin === 'http://127.0.0.1:5500'
+
+// if (!isProCodrrPlayer) {
+//   document.querySelector('#player').classList.add('not-procodrr-player')
+// }
